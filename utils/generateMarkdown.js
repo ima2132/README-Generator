f2cc4ad2c8ff807535a624fea94958ddaa552acc
ima2132 <ima2132@columbia.edu>
2023-05-21 14:04:43 -0400

    worked on functions to return the license badge/link

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,17 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+// a function to return the license badge
+function renderLicenseBadge(license) {
+  if (license !== 'None') {
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  }
+  return '';
 }
 
-module.exports = generateMarkdown;
+// a function to return the license link
+function renderLicenseLink(license) {
+  if (license !== 'None') {
+    return (
+      `\n* [License](#license)\n`
+    );
+  }
+  return '';
+}
