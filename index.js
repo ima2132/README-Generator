d7c72c3f4743d2d9597c6a646c984d8b0c932c35
ima2132 <ima2132@columbia.edu>
2023-05-21 13:45:18 -0400

    added rest of the questions; need to add functions

--- index.js
+++ index.js
@@ -25,4 +25,32 @@ const questions = [
         name: 'description',
         message: 'Please write a short description of your project',
     },
-]
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license should your project have?',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What command should be run to install dependencies?',
+        default: 'npm i',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What command should be run to run tests?',
+        default: 'npm test',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What does the user need to know about using the repo?',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'What does the user need to know about contributing to the repo?',
+    },
+];
