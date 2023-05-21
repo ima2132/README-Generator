e10200015d81140ff3a8d2aae57ba1cefaea13fa
ima2132 <ima2132@columbia.edu>
2023-05-21 14:16:11 -0400

    added to generateMarkdown; may need to reword the license portion in a bit

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -15,3 +15,33 @@ function renderLicenseLink(license) {
   }
   return '';
 }
+
+// A function to return the license section
+function renderLicenseSection(license) {
+  if (license !== 'None') {
+    return (
+      `## License\nThis project is licensed under the ${license} license.`
+    );
+  }
+  return '';
+}
+
+// A function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+${renderLicenseBadge(data.license)}
+
+## Description 
+
+${data.description}
+
+## Table of Contents 
+
+* [Installation](#installation)
+* [Usage](#usage)
+${renderLicenseLink(data.license)}
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
