f930f4ec6341e679237e4e9bc559973d0bb68454
ima2132 <ima2132@columbia.edu>
2023-05-21 13:38:14 -0400

    installed inquirer; added an array of questions; still need to add more

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-// TODO: Include packages needed for this application
+//include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+// an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: "What is your project's name?",
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a short description of your project',
+    },
+]
